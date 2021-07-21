46003674d40bb76f9a443db5c1a7640c50ab5993
Peter Chang <JyChang012@outlook.com>
2021-07-21 10:14:50 +0800

    finish w4a

--- src/w4/c/A.java
+++ src/w4/c/A.java
@@ -1,75 +1,18 @@
 package w4.c;
 
-import java.io.LineNumberReader;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.Scanner;
+
+// see notes
 
 public class A {
 
-
-    public static long char2digit(char c) {
+    public static int char2digit(char c) {
         return c - '0';
     }
 
 
-    public static long solveBox(long wide, long height) {
-        /*
-        int[] repeat = new int[wide + 1];
-        for (int i = 1; i <= wide; i++) {
-            if (wide - i + 1 > height) {
-                repeat[i] = 0;
-            } else {
-                int temp = height;
-                for (int j = 1; j < wide - i + 1; j++) {
-                    temp *= (height - j);
-                }
-                repeat[i] = temp;
-            }
-        }
-         */
-        if (wide == 1) {
-            return height;
-        } else if (height == 0) {
-            return 0;
-        }
-
-
-        long ret = 0;
-        for (int i = 1; i <= wide; i++) {
-            if (wide - i + 1 <= height) {
-                long temp = height;
-                for (int j = 1; j < wide - i + 1; j++) {
-                    temp *= (height - j);
-                }
-                ret += temp;
-            }
-        }
-        return ret;
-
-    }
-
-
-    public static long calculateRamp(long[] arr) {
-        int i = arr.length - 2, wide = 1;
-        long prev = arr[arr.length - 1];
-        long ret = 0;
-        while (i >= 0) {
-            long diff = prev - arr[i];
-            if (diff != 0) {
-                ret += solveBox(wide, diff + 1);
-                prev = arr[i];
-            }
-            wide += 1;
-            i--;
-        }
-        if (ret == 0)
-            return solveBox(arr.length, arr[0] + 1);
-        else
-            return ret;
-    }
-
-
-    public static boolean checkRamp(long[] arr) {
+    public static boolean checkRamp(int[] arr) {
         long last = Long.MIN_VALUE;
         for (long cur: arr) {
             if (cur < last)
@@ -81,6 +24,47 @@ public class A {
     }
 
 
+    public static long calculateRamp(int[] digits) {
+
+        long[][] cache = new long[digits.length][10];
+        for (long[] ca: cache)
+            Arrays.fill(ca, -1);
+
+        return solveUpperBound(digits.length - 1, 0, digits, cache);
+    }
+
+    public static long solve(int i, int d, long[][] cache) { // starting from 0
+
+        if (i == 0) {
+            return 9 - d + 1;
+        }
+
+        if (cache[i][d] >= 0) {
+            return cache[i][d];
+        }
+
+        long ret = 0;
+        for (int dd = d; dd < 10; dd++) {
+            ret += solve(i - 1, dd, cache);
+        }
+        cache[i][d] = ret;
+        return ret;
+    }
+
+    public static long solveUpperBound(int i, int d, int[] digits, long[][] cache) { // starting from 0
+        if (i == 0) {
+            return digits[digits.length - 1] - d;
+        }
+
+        long ret = 0;
+        for (int dd = d; dd < digits[digits.length - i - 1]; dd++) {
+            ret += solve(i - 1, dd, cache);
+        }
+        ret += solveUpperBound(i - 1, digits[digits.length - i - 1], digits, cache);
+        return ret;
+    }
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
@@ -89,7 +73,7 @@ public class A {
 
         for (int i = 0; i < n; i++) {
             char[] cA = scanner.nextLine().toCharArray();
-            long[] digits = new long[cA.length];
+            int[] digits = new int[cA.length];
             for (int j = 0; j < digits.length; j++) {
                 digits[j] = char2digit(cA[j]);
             }
---
+++ src/w4/c/B.java
@@ -0,0 +1,18 @@
+package w4.c;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class B {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt(), k = scanner.nextInt();
+        scanner.nextLine();
+        int[] locs = new int[n];
+        for (int i = 0; i < n; i++) {
+            locs[i] = scanner.nextInt();
+        }
+
+
+    }
+}
